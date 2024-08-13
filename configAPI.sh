e6c40e911972713e9c421ac8e4d0ec77d98b706c
Caleb Krauter <ghostMe66+github@proton.me>
2024-08-13 10:12:56 -0700

    Delete configAPI.sh

--- configAPI.sh
+++
@@ -1,3 +0,0 @@
-# Generated from Claude AI.
-echo Configuring API Url as an enviornment variable for the current session.
-export API_URL=$(aws ssm get-parameter --name /myapp/api-url --query Parameter.Value --output text)
